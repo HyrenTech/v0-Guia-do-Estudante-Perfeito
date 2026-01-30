ced794a17186fb7d7b62f2ef25a27c7f105cec4d
L7X-Git <mx.l7x.01@gmail.com>
2026-01-30 17:45:08 -0300

    Add Meta Pixel and update deps

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,9 +60,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" className="scroll-smooth">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');fbq('init', '859690886971372');fbq('track', 'PageView');`,
+          }}
+        />
+      </head>
       <body
         className={`${bodyFont.variable} ${displayFont.variable} ${monoFont.variable} ${robotoFont.variable} font-sans antialiased`}
       >
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=859690886971372&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         {children}
         <Analytics />
       </body>
